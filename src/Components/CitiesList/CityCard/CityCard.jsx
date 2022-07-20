a9453c4dca35480cc9b03a451f406556c7ea75eb
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-07-20 21:29:17 +0600

    added Modal window

--- src/Components/CitiesList/CityCard/CityCard.jsx
+++ src/Components/CitiesList/CityCard/CityCard.jsx
@@ -3,6 +3,7 @@ import styles from './CityCard.module.scss'
 import {CitiesContext} from "../../../CitiesContext/CitiesContext";
 import {useEffect, useState, useContext} from "react";
 import {Link} from "react-router-dom";
+import Modal from "../../../UI/Modal/Modal";
 
 ;
 
@@ -61,7 +62,6 @@ const CityCard = (props) => {
 
     const removeCityHandle = (event) => {
         event.preventDefault();
-        console.log(props.city)
         dispatchCities({
             type: 'REMOVE_CITY',
             city: props.city
@@ -69,27 +69,33 @@ const CityCard = (props) => {
     }
 
     return (
-        <Link to={`/city/${props.city}`} state={{data: data, city: props.city}}>
-            <div className={styles.city}>
-                <div className={styles.actions}>
-                    <button onClick={changeCityHandler}>edit</button>
-                    <button onClick={removeCityHandle}>X</button>
-                </div>
-                <div>
-                    <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" className={styles.icon}/>
-                    {isEdit && <div>
+        <React.Fragment>
+            {isEdit && (
+                <Modal>
+                    <div>
                         <input className={styles.inputChange} type="text" ref={newCityRef}/>
                         <div className={styles.changeActions}>
                             <button className={styles.btnDone} onClick={changeCityDoneHandler}>✔</button>
                             <button className={styles.btnDone} onClick={changeCityCancelHandler}>X</button>
                         </div>
-                    </div>}
-                    {!isEdit && <h2 className={styles.cityName}>{props.city}</h2>}
-                    <p className={styles.description}>{description}</p>
-                    <span className={styles.temp}>{Math.round(temp)} &#8451;</span>
+                    </div>
+                </Modal>
+            )}
+            <Link to={`/city/${props.city}`} state={{data: data, city: props.city}}>
+                <div className={styles.city}>
+                    <div className={styles.actions}>
+                        <button onClick={changeCityHandler}>edit</button>
+                        <button onClick={removeCityHandle}>X</button>
+                    </div>
+                    <div>
+                        <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" className={styles.icon}/>
+                        <h2 className={styles.cityName}>{props.city}</h2>
+                        <p className={styles.description}>{description}</p>
+                        <span className={styles.temp}>{Math.round(temp)} &#8451;</span>
+                    </div>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </React.Fragment>
     );
 };
 
