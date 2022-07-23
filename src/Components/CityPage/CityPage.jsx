691857ab486ec944d92e8e7f1b90699f18c682f8
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-07-23 23:31:32 +0600

    the 'back to home' button has been added to the city page

--- src/Components/CityPage/CityPage.jsx
+++ src/Components/CityPage/CityPage.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+// import {useHistory} from 'react-router-dom'
 import styles from './CityPage.module.scss'
 
-import { useLocation } from "react-router-dom";
+import {useNavigate, useLocation} from "react-router-dom";
 
 import CurrentInfo from "./CurrentInfo/CurrentInfo";
 import DailyList from "./DailyList/DailyList";
 
 const CityPage = (props) => {
     const location = useLocation();
+    const navigate = useNavigate();
+
     console.log(location.state.data)
     return (
         <React.Fragment>
+            <button className={styles.btnGoBack} onClick={() => navigate('/')}>Назад ⬅</button>
             <CurrentInfo data={location.state.data.current} city={location.state.city} />
             <DailyList data={location.state.data.daily} />
         </React.Fragment>
